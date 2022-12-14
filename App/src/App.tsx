4dcff47c1ac80f68dd5b64ae89d7b63d6b54ca4a
Artem Mokhov <teumik@gmail.com>
2022-12-14 18:30:06 +0400

    test

---
+++ App/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.scss';
+import Header from './components/header/header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <h1>Header More Headers And I Like It</h1>
+      <p>Text</p>
+      <section>
+        <h2>Subheader</h2>
+        <p>Subtext</p>
+      </section>
+    </div>
+  );
+}
+
+export default App;
---
+++ App/src/components/header/header.tsx
@@ -0,0 +1,12 @@
+import './header.scss';
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__logo">Logo</div>
+      <div className="header__cart">Cart</div>
+    </div>
+  );
+}
+
+export default Header;
